cdcae16f5a76f15e34918c6b0badbf05b9a5ca26
adi alam <adikuasa.mangkualam@ist.id>
2024-05-02 16:26:24 +0700

    fixing all database connection to one page

--- admin/ajax-grid-data3.php
+++ admin/ajax-grid-data3.php
@@ -1,9 +1,6 @@
 <?php
 /* Database connection start */
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "tiket_db";
+include "../database_konek.php";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 $requestData = $_REQUEST;
--- admin/ajax-grid-datagsuite.php
+++ admin/ajax-grid-datagsuite.php
@@ -1,10 +1,7 @@
 <?php
 /* Database connection start */
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "tiket_db";
+include "../database_konek.php";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 
